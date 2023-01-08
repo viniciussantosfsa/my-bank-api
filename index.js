de5a3783318f9fbfc4e057b091a0f5b0e3b6a3d6
Viníciusfsa <viniciussantos123fsa@gmail.com>
2023-01-07 21:23:49 -0300

    auth

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import winston from "winston";
 import accountsRouter from "./routes/accounts.routes.js";
 import cors from "cors";
+import expressBasicAuth from "express-basic-auth";
 
 import { promises as fs } from "fs";
 const { readFile, writeFile } = fs;
@@ -26,7 +27,49 @@ global.logger = winston.createLogger({
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use("/account", accountsRouter);
+// Auth
+function getRole(username) {
+  if (username == "admin") {
+    return "admin";
+  } else if (username == "kellvy") {
+    return "role1";
+  }
+}
+
+function authorize(...allowed) {
+  const isAllowed = (role) => allowed.indexOf(role) > -1;
+
+  return (req, res, next) => {
+    if (req.auth.user) {
+      const role = getRole(req.auth.user);
+
+      if (isAllowed(role)) {
+        next();
+      } else {
+        res.status(401).send("Role not allowed");
+      }
+    } else {
+      res.status(403).send("User not found");
+    }
+  };
+}
+
+app.use(
+  expressBasicAuth({
+    authorizer: (username, password) => {
+      
+      const userMatches = expressBasicAuth.safeCompare(username, "admin");
+      const pwdMatches = expressBasicAuth.safeCompare(password, "admin");
+
+      const userMatches2 = expressBasicAuth.safeCompare(username, "kellvy");
+      const pwdMatches2 = expressBasicAuth.safeCompare(password, "1234");
+
+      return userMatches && pwdMatches || userMatches2 && pwdMatches2;
+    },
+  })
+);
+//
+app.use("/account", authorize("admin", "role1"), accountsRouter);
 
 app.listen(port, async () => {
   try {
